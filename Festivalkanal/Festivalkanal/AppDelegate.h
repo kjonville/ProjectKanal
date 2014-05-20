44bd953d469586e9cafc742dfe392262fb6c835c
kjonville <kenneth.jonville@student.ehb.be>
2014-05-20 11:56:13 +0200

    v1.0

---
+++ Festivalkanal/Festivalkanal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Festivalkanal
+//
+//  Created by Kenneth Jonville on 20/05/14.
+//  Copyright (c) 2014 Kenneth Jonville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Festivalkanal/Festivalkanal/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Festivalkanal
+//
+//  Created by Kenneth Jonville on 20/05/14.
+//  Copyright (c) 2014 Kenneth Jonville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
